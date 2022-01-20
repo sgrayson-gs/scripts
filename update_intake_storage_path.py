8b3d802c2a7d276383511565a0dd8660a5e02b8b
sam <sam.grayson@groundspeed.com>
2022-01-20 11:54:56 -0500

    update agency_configuration data

--- update_intake_storage_path.py
+++ update_intake_storage_path.py
@@ -1,80 +1,74 @@
 """
 Simple script to to update the intake_storage path in postgres using the screts value.
 
-Prior to running the script set the following variables:
-- STAGE
-- INTERNAL_API_ENDPOINT
+Requirements:
+- You must be on VPN
+- set the STAGE environment, default to dev
 """
 
 from gs_common.config.cryptography_manager import CryptoManager
-from database.operations.agency_config import get_agency_configuration_by_api_key
-
-# from typing import Optional
+from database_model.db import session_scope
+from database_model.schemas import AgencyConfiguration
 
 import os
 import json
 import boto3
-import requests
 
-client = boto3.client('secretsmanager')
-
+client = boto3.client("secretsmanager")
 STAGE = os.getenv("STAGE", "dev")
-INTERNAL_API_ENDPOINT = os.getenv("INTERNAL_API_ENDPOINT", None)
 
-def update_intake_storage_path(path, api_key):
+def update_intake_storage_path(path, api_key, session):
     """
     Takes in a hashed api & folder path. Stores the path in the agency_configuration table in the intake_storage path
     """
 
     if not path or not api_key:
         return None
-    else:
-        hashed_api_key = (
-            CryptoManager("intake-processor", STAGE)
-            .hash("api_key_hash_salt", api_key)
-            .decode("utf-8")
-        )
 
-        updated_path = path.replace(".", "/")
+    hashed_api_key = (
+        CryptoManager("intake-processor", STAGE)
+        .hash("api_key_hash_salt", api_key)
+        .decode("utf-8")
+    )
 
-        print(hashed_api_key, updated_path)
+    updated_path = path.replace(".", "/")
 
-        # Need a way to login to dev db...
-        agency_configuration = get_agency_configuration_by_api_key(hashed_api_key)
-        
-        update_json = {
-            "agencyConfigurationId": "1",
-            "data": {"intakeStoragePath": updated_path}
-        }
+    print(hashed_api_key, updated_path)
 
+    try:
+        session.query(AgencyConfiguration).\
+            filter(AgencyConfiguration.hashed_api_key == hashed_api_key).\
+            update({"intake_storage_path": updated_path})
+    except Exception as e:
+        print(f"Error updating the agency_configuration for hashed_key {hashed_api_key}: {e}")
 
-        # response = requests.patch(
-        #     url=f"{INTERNAL_API_ENDPOINT}/api/v2/upload", json=update_json,
-        # )
-        # if response.status_code == 200:
-        #     print(f"Error updating the following path wish hash: {hashed_api_key}")
+    return True
 
-        return True
 
 def run():
+    # Get the mapping of intake paths/api_keys
     secret_response = client.get_secret_value(
-        SecretId = f"{STAGE}-intake-sftp-api-key-lookup" 
+        SecretId=f"{STAGE}-intake-sftp-api-key-lookup"
     )
 
     if secret_response["ResponseMetadata"]["HTTPStatusCode"] == 200:
         secret_json = json.loads(secret_response["SecretString"])
-        for path, api_key in secret_json.items():
-            update_intake_storage_path(path, api_key)
+        with session_scope() as session:
+            try:
+                # Loop through each of the keys and update the path in postgres
+                for path, api_key in secret_json.items():
+                    update_intake_storage_path(path, api_key, session)
+
+                # Commit the updates to postgres
+                session.commit()
+
+                print(f"-- Script complete. Records updated {len(secret_json)} --")
+            finally:
+                session.expunge_all()
     else:
         print("Error fetching aws secret")
 
 
-
-
 if __name__ == "__main__":
     # Do the thang.
-    if not INTERNAL_API_ENDPOINT:
-        print("Set INTERNAL_API_ENDPOINT environment variable before running script.")
     run()
-
-
